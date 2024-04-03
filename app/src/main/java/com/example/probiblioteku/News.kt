4fced07076475566652ad42a5f5ea0dbcced8d1f
Natshark <modin.ru2@mail.ru>
2024-04-03 23:57:10 +0500

    parser of news

---
+++ app/src/main/java/com/example/probiblioteku/News.kt
@@ -0,0 +1,7 @@
+package com.example.probiblioteku
+
+data class News(
+    val title: String,
+    val imageSrc: String,
+    val href: String,
+)
--- app/src/main/java/com/example/probiblioteku/NewsActivity.kt
+++ app/src/main/java/com/example/probiblioteku/NewsActivity.kt
@@ -4,55 +4,48 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
-
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import android.annotation.SuppressLint
+import android.content.Context
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import androidx.recyclerview.widget.LinearLayoutManager
 import org.jsoup.Jsoup
-import java.io.IOException
 
 class NewsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
 
-        val buttonProfile: ImageView = findViewById(R.id.buttonProfile)
-        buttonProfile.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
-        val buttonBooking: ImageView = findViewById(R.id.buttonBooking)
-        buttonBooking.setOnClickListener {
-            val intent = Intent(this, BookingActivity::class.java)
-            startActivity(intent)
-        }
-        val buttonNews: ImageView = findViewById(R.id.buttonNews)
-        buttonNews.setOnClickListener {
-            val intent = Intent(this, NewsActivity::class.java)
-            startActivity(intent)
-        }
-
-
-
         runBlocking {
-            // Запуск корутины для выполнения сетевого запроса
             withContext(Dispatchers.IO) {
-                // URL сайта для парсинга
                 val url = "https://lib.ugrasu.ru/"
+                val document = Jsoup.connect(url).get()
+                val titles = document.select(".news-title")
+                val urlImages = document.select(".preview_picture")
 
-                try {
-                    // Получение объекта Document, представляющего веб-страницу
-                    val document = Jsoup.connect(url).get()
-
-                    // Получение заголовка сайта (тег <title>)
-                    val title = document.title()
-
-                    // Вывод заголовка в консоль
-                    println("Заголовок сайта: $title")
-                } catch (e: IOException) {
-                    // Обработка ошибок, связанных с подключением к сайту
-                    println("Ошибка при подключении к сайту: ${e.message}")
+                val newsList = mutableListOf<News>()
+                for (i in 0 until 4)
+                {
+                    val title = titles[i].text()
+                    val imageSrc = "https://lib.ugrasu.ru${urlImages[i].attr("src")}"
+                    val href = "https://lib.ugrasu.ru${titles[i].select("a").first()?.attr("href")}"
+                    newsList.add(News(title, imageSrc, href))
                 }
+
+                val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+                val adapter = NewsAdapter(newsList)
+                recyclerView.adapter = adapter
+                recyclerView.layoutManager = LinearLayoutManager(this@NewsActivity)
             }
         }
 
