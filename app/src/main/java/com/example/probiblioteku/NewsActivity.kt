7dd8949093042ae9e5b04f7385520dc31642c1ad
danyakovalugrasu <danyakovalugrasu@gmail.com>
2024-04-05 17:04:46 +0500

    fix nav bar

--- app/src/main/java/com/example/probiblioteku/NewsActivity.kt
+++ app/src/main/java/com/example/probiblioteku/NewsActivity.kt
@@ -1,23 +1,14 @@
 package com.example.probiblioteku
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
-import android.annotation.SuppressLint
-import android.content.Context
-import android.webkit.WebView
-import android.webkit.WebViewClient
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
-import com.squareup.picasso.Picasso
-import androidx.recyclerview.widget.LinearLayoutManager
 import org.jsoup.Jsoup
 
 class NewsActivity : AppCompatActivity() {
@@ -25,6 +16,23 @@ class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        // navbar
+        val buttonProfile: ImageView = findViewById(R.id.buttonProfile)
+        buttonProfile.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        val buttonBooking: ImageView = findViewById(R.id.buttonBooking)
+        buttonBooking.setOnClickListener {
+            val intent = Intent(this, BookingActivity::class.java)
+            startActivity(intent)
+        }
+        val buttonNews: ImageView = findViewById(R.id.buttonNews)
+        buttonNews.setOnClickListener {
+            val intent = Intent(this, NewsActivity::class.java)
+            startActivity(intent)
+        }
 
         runBlocking {
             withContext(Dispatchers.IO) {
